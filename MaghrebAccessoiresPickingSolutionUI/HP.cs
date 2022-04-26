7ee38e2ecaa68083ed77047e6fd71d1359ed42ff
BELAKBIR SAID <62822346+saidbelakbir@users.noreply.github.com>
2022-04-26 05:33:59 +0000

    zddqz

--- MaghrebAccessoiresPickingSolutionUI/HP.cs
+++ MaghrebAccessoiresPickingSolutionUI/HP.cs
@@ -24,7 +24,15 @@ namespace MaghrebAccessoiresPickingSolutionUI
 
             DataTable T0 = new DataTable();
             DataTable T1 = new DataTable();
+            DataTable T3 = new DataTable();
 
+
+            string mag = this.comboBox1.Text;
+            string sqlQuery;
+
+           
+
+           
 
             try
             {
@@ -33,21 +41,63 @@ namespace MaghrebAccessoiresPickingSolutionUI
              
                         using (SqlConnection connection = new SqlConnection(connectionString))
                         {
-                            string sqlQuery = "select * from Hmap";
+
+                    if (mag == "1")
+                    {
+
+                        sqlQuery = "select * from Table_4HP1";
+                        SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+
+                        DataTable dtbl = new DataTable();
+
+
+                        dataAdapter.Fill(dtbl);
+                        T0 = dtbl;
+                        //dataGridView1.DataSource = dtbl;
+
+
+                    }
+                    if (mag == "2") { sqlQuery = "select * from Table_4HP2";
+
+                        SqlCommand command1 = new SqlCommand(sqlQuery, connection);
+
+
+                        SqlDataAdapter dataAdapter1 = new SqlDataAdapter(command1);
+
+
+                        DataTable dtbl1 = new DataTable();
+
+
+                        dataAdapter1.Fill(dtbl1);
+                        T0 = dtbl1;
+                       // dataGridView1.DataSource = dtbl1;
+                    }
+
+                    if (mag == "3") { sqlQuery = "select * from Table_4HP1";
+
+                        SqlCommand command2 = new SqlCommand(sqlQuery, connection);
+
+
+                        SqlDataAdapter dataAdapter2 = new SqlDataAdapter(command2);
+
+
+                        DataTable dtbl2 = new DataTable();
+
+
+                        dataAdapter2.Fill(dtbl2);
+                        T0 = dtbl2; 
+                        //dataGridView1.DataSource = dtbl2;
+
+
+                    }
+                    dataGridView1.DataSource = T0;
                     
 
-                            SqlCommand command = new SqlCommand(sqlQuery, connection);
 
-
-                            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
-
-
-                            DataTable dtbl = new DataTable();
-
-
-                            dataAdapter.Fill(dtbl);
-                            dataGridView1.DataSource = dtbl;
-                    T0 = dtbl;
 
 
 
@@ -57,26 +107,47 @@ namespace MaghrebAccessoiresPickingSolutionUI
 
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    string sqlQuery = "select * from Table_1";
+                    string sqlQueryA = "select * from Table_1";
 
 
-                    SqlCommand command = new SqlCommand(sqlQuery, connection);
+                    SqlCommand commandA = new SqlCommand(sqlQueryA, connection);
 
 
-                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    SqlDataAdapter dataAdapterA = new SqlDataAdapter(commandA);
 
 
-                    DataTable dtbl1= new DataTable();
+                    DataTable dtbl1A= new DataTable();
 
 
-                    dataAdapter.Fill(dtbl1);
-                    dataGridView1.DataSource = dtbl1;
-                    T1 = dtbl1;
-
-
-
+                    dataAdapterA.Fill(dtbl1A);
+                    //dataGridView1.DataSource = dtbl1A;
+                    T1 = dtbl1A;
 
                 }
+
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string sqlQueryA1 = "select * from siteAP";
+
+
+                    SqlCommand commandA1 = new SqlCommand(sqlQueryA1, connection);
+
+
+                    SqlDataAdapter dataAdapterA1 = new SqlDataAdapter(commandA1);
+
+
+                    DataTable dtbl1A1 = new DataTable();
+
+
+                    dataAdapterA1.Fill(dtbl1A1);
+                   // dataGridView1.DataSource = dtbl1A1;
+                    T3 = dtbl1A1;
+
+                }
+
+
+
 
             }
 
@@ -90,13 +161,54 @@ namespace MaghrebAccessoiresPickingSolutionUI
             try
             {
 
-               for (int i = 0; i < T0.Columns.Count; i++)
+                string emp = "";
+
+                for (int i = 0; i < T0.Columns.Count; i++)
                 {
                     for (int j = 0; j < T0.Rows.Count; j++)
                     {
-                        string key = T0.Rows[j][i].ToString(); 
+                        string key = T0.Rows[j][i].ToString();
+                        string expression = "EmplacementOpt like '%" + key+"%'";
+                        DataRow[] foundRows;
+                        
+
+                        // Use the Select method to find all rows matching the filter.
+                        foundRows = T1.Select(expression);
+                        if (foundRows.Length != 0)
+                        { 
+                            
+                            string EmpO = foundRows[0]["EmplacementAct"].ToString();
+                            string classeO = foundRows[0]["Classe"].ToString();
+                            emp = EmpO;
+                            this.label1.Text = emp;
+                        }
+
+                        string expression1 = "Emp like '%" + emp + "%'";
+                        DataRow[] foundRows1;
+
+                        // Use the Select method to find all rows matching the filter.
+                        foundRows1 = T3.Select(expression1);
+
+                        if (foundRows1.Length != 0)
+                        {
+
+                            //string  = foundRows1[0]["EmplacementAct"].ToString();
+                            string classe1 = foundRows1[0]["Classe"].ToString();
+                            //emp = EmpO;
+                            this.label1.Text = classe1;
+                            if (classe1 == "A") { dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.Red; }
+
+                            if (classe1 == "B") { dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.Yellow; }
+                            if (classe1 == "C") { dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.Lime; }
+
+                        }
 
 
+
+                        //cloring datagridView 
+
+                       
+                           
 
 
 
